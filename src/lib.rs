c6ad24f1661f46d9f40a9b1fe94b6eb4a5ae8b6c
swlkr <swlkr.rbl@gmail.com>
2023-08-11 18:03:26 -0600

    Add columns in sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
-use std::time::Duration;
-
 use sqlx::{
     query_as,
     sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions, SqliteSynchronous},
     Executor, FromRow, SqlitePool,
 };
+use std::time::Duration;
 
-#[derive(Default)]
+#[derive(FromRow)]
+struct ColumnDef {
+    table_name: String,
+    name: String,
+    data_type: String,
+    not_null: bool,
+    primary_key: bool,
+    default_value: String,
+}
+
+#[derive(Default, PartialEq, Debug)]
 struct Column {
+    table_name: String,
     default_value: Option<String>,
     name: String,
     not_null: bool,
@@ -16,7 +26,7 @@ struct Column {
 }
 
 impl Column {
-    fn to_sql(&self) -> String {
+    fn definition_sql(&self) -> String {
         let not_null = match self.not_null {
             true => Some("not null"),
             false => None,
@@ -42,6 +52,50 @@ impl Column {
         .collect::<Vec<_>>()
         .join(" ")
     }
+
+    fn add_column_sql(&self) -> String {
+        format!(
+            "alter table {} add column {};",
+            self.table_name,
+            self.definition_sql()
+        )
+    }
+
+    fn full_name(&self) -> String {
+        format!("{} {}", self.table_name, self.name)
+    }
+}
+
+impl From<ColumnDef> for Column {
+    fn from(value: ColumnDef) -> Self {
+        let ColumnDef {
+            table_name,
+            name,
+            data_type,
+            not_null,
+            primary_key,
+            default_value,
+        } = value;
+        let default_value = if default_value.is_empty() {
+            None
+        } else {
+            Some(default_value)
+        };
+        let data_type = match data_type.to_ascii_lowercase().as_ref() {
+            "text" => sqlite::DataType::Text,
+            "integer" => sqlite::DataType::Integer,
+            "real" => sqlite::DataType::Real,
+            _ => sqlite::DataType::Blob,
+        };
+        Column {
+            table_name,
+            name,
+            data_type,
+            not_null,
+            primary_key,
+            default_value,
+        }
+    }
 }
 
 #[derive(Default)]
@@ -57,10 +111,6 @@ trait Table {
     fn indexes(&self) -> Vec<Index>;
     fn create_table_sql(&self) -> String;
     fn drop_table_sql(&self) -> String;
-    fn add_column_sql(&self, column: Column) -> String;
-    fn drop_column_sql(&self, column: Column) -> String;
-    fn create_index_sql(&self, index: Index) -> String;
-    fn drop_index_sql(&self, index: Index) -> String;
 }
 
 pub mod sqlite {
@@ -71,7 +121,7 @@ pub mod sqlite {
     pub type Index = &'static str;
     pub type UniqueIndex = &'static str;
 
-    #[derive(Default)]
+    #[derive(Default, PartialEq, Debug)]
     pub enum DataType {
         #[default]
         Blob,
@@ -128,24 +178,28 @@ impl Table for Users {
                 name: "id".to_string(),
                 primary_key: true,
                 data_type: sqlite::DataType::Integer,
+                table_name: self.name(),
                 ..Default::default()
             },
             Column {
                 name: "name".to_string(),
                 not_null: true,
                 data_type: sqlite::DataType::Text,
+                table_name: self.name(),
                 ..Default::default()
             },
             Column {
                 name: "created_at".to_string(),
                 data_type: sqlite::DataType::Real,
                 not_null: true,
+                table_name: self.name(),
                 ..Default::default()
             },
             Column {
                 name: "updated_at".to_string(),
                 data_type: sqlite::DataType::Real,
                 not_null: true,
+                table_name: self.name(),
                 ..Default::default()
             },
         ]
@@ -167,29 +221,13 @@ impl Table for Users {
         let columns_sql = self
             .columns()
             .iter()
-            .map(|c| c.to_sql())
+            .map(|c| c.definition_sql())
             .collect::<Vec<_>>()
             .join(", ");
         format!("create table {} ({});", self.name(), columns_sql)
     }
 
     fn drop_table_sql(&self) -> String {
-        todo!()
-    }
-
-    fn add_column_sql(&self, column: Column) -> String {
-        todo!()
-    }
-
-    fn drop_column_sql(&self, column: Column) -> String {
-        todo!()
-    }
-
-    fn create_index_sql(&self, index: Index) -> String {
-        todo!()
-    }
-
-    fn drop_index_sql(&self, index: Index) -> String {
         todo!()
     }
 }
@@ -221,32 +259,16 @@ impl Table for TableName {
     fn drop_table_sql(&self) -> String {
         format!("drop table {};", self.0)
     }
-
-    fn add_column_sql(&self, column: Column) -> String {
-        todo!()
-    }
-
-    fn drop_column_sql(&self, column: Column) -> String {
-        todo!()
-    }
-
-    fn create_index_sql(&self, index: Index) -> String {
-        todo!()
-    }
-
-    fn drop_index_sql(&self, index: Index) -> String {
-        todo!()
-    }
 }
 
 fn tables_to_create<'a>(
     db_table_names: &'a Vec<TableName>,
     tables: &'a Vec<impl Table>,
 ) -> Vec<&'a impl Table> {
-    let sql_table_names: Vec<_> = db_table_names.iter().map(|table| &table.0).collect();
+    let db_table_names: Vec<_> = db_table_names.iter().map(|table| &table.0).collect();
     tables
         .iter()
-        .filter(|t| !sql_table_names.contains(&&t.name()))
+        .filter(|t| !db_table_names.contains(&&t.name()))
         .collect::<Vec<_>>()
 }
 
@@ -273,6 +295,29 @@ fn drop_tables_sql(table_names: Vec<&TableName>) -> String {
     table_names
         .iter()
         .map(|tn| tn.drop_table_sql())
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+fn columns_to_add(db_columns: Vec<Column>, code_columns: Vec<Column>) -> Vec<Column> {
+    let db_column_names = db_columns.iter().map(|c| c.full_name()).collect::<Vec<_>>();
+    dbg!(&db_column_names);
+    let code_column_names = &code_columns
+        .iter()
+        .map(|c| c.full_name())
+        .collect::<Vec<_>>();
+    dbg!(&code_column_names);
+    let result = code_columns
+        .into_iter()
+        .filter(|c| !db_column_names.contains(&c.full_name()))
+        .collect();
+    result
+}
+
+fn add_columns_sql(columns: Vec<Column>) -> String {
+    columns
+        .iter()
+        .map(|c| c.add_column_sql())
         .collect::<Vec<_>>()
         .join("\n")
 }
@@ -317,6 +362,14 @@ impl Database {
         }
     }
 
+    async fn columns(&self) -> Vec<Column> {
+        let column_defs: Vec<ColumnDef> = sqlx::query_as("select s.name as table_name, pti.name as name, pti.type as data_type, pti.pk as primary_key, pti.dflt_value as default_value, pti.[notnull] as not_null from sqlite_schema s left outer join pragma_table_info((s.name)) pti on pti.name <> s.name where s.type = 'table'").fetch_all(&self.pool).await.unwrap();
+        column_defs
+            .into_iter()
+            .map(|cd| cd.into())
+            .collect::<Vec<_>>()
+    }
+
     async fn execute(&self, sql: &str) -> Result<(), sqlx::Error> {
         let _ = self.pool.execute(sql).await?;
         Ok(())
@@ -330,6 +383,11 @@ impl Database {
         let tables_to_drop = tables_to_drop(&table_names, &tables);
         let drop_tables_sql = drop_tables_sql(tables_to_drop);
         let _ = self.execute(&drop_tables_sql).await?;
+        let db_columns = self.columns().await;
+        let new_columns = tables.iter().flat_map(|t| t.columns()).collect::<Vec<_>>();
+        let columns_to_add = columns_to_add(db_columns, new_columns);
+        let add_columns_sql = add_columns_sql(columns_to_add);
+        let _ = self.execute(&add_columns_sql).await?;
         Ok(())
     }
 }
@@ -337,6 +395,10 @@ impl Database {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    async fn db() -> Database {
+        Database::new("sqlite://:memory:".to_string()).await
+    }
 
     #[test]
     fn create_tables_sql_works() {
@@ -363,7 +425,7 @@ mod tests {
 
     #[tokio::test]
     async fn db_table_names_with_existing_table_works() {
-        let db = Database::new("sqlite://:memory:".to_string()).await;
+        let db = db().await;
         let sql = create_tables_sql(vec![&Users::new()]);
         let _ = db.execute(&sql).await;
         let names: Vec<_> = db.table_names().await.into_iter().map(|tn| tn.0).collect();
@@ -399,13 +461,131 @@ mod tests {
     async fn sync_with_drop_tables_works() -> Result<(), sqlx::Error> {
         let users = Users::new();
         let tables = vec![users];
-        let db = Database::new("sqlite://:memory:".to_string()).await;
+        let db = db().await;
         let _ = db.sync(tables).await?;
         let table_names = db.table_names().await;
         assert_eq!(1, table_names.len());
         let _ = db.sync(empty_tables()).await?;
         let table_names = db.table_names().await;
         assert!(table_names.is_empty());
+        Ok(())
+    }
+
+    // #[derive(Table)]
+    // #[rizzle(table_name = "table_a")]
+    struct TableA {
+        a: sqlite::Text,
+    }
+
+    impl Table for TableA {
+        fn new() -> Self {
+            Self { a: "a" }
+        }
+
+        fn name(&self) -> String {
+            "table_a".to_string()
+        }
+
+        fn columns(&self) -> Vec<Column> {
+            vec![Column {
+                table_name: self.name(),
+                name: "a".to_owned(),
+                data_type: sqlite::DataType::Text,
+                ..Default::default()
+            }]
+        }
+
+        fn indexes(&self) -> Vec<Index> {
+            todo!()
+        }
+
+        fn create_table_sql(&self) -> String {
+            format!(
+                "create table {} ({});",
+                self.name(),
+                self.columns()
+                    .iter()
+                    .map(|c| c.definition_sql())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+        }
+
+        fn drop_table_sql(&self) -> String {
+            format!("drop table {}", self.name())
+        }
+    }
+
+    //#[derive(Table)]
+    // #[rizzle(table_name = "table_a")]
+    struct TableA2 {
+        a: sqlite::Text,
+        b: sqlite::Text,
+    }
+
+    impl Table for TableA2 {
+        fn new() -> Self {
+            Self { a: "a", b: "b" }
+        }
+
+        fn name(&self) -> String {
+            // this comes from rizzle(table_name = "table_a") attribute
+            "table_a".to_string()
+        }
+
+        fn columns(&self) -> Vec<Column> {
+            vec![
+                Column {
+                    table_name: self.name(),
+                    name: "a".to_owned(),
+                    data_type: sqlite::DataType::Text,
+                    ..Default::default()
+                },
+                Column {
+                    table_name: self.name(),
+                    name: "b".to_owned(),
+                    data_type: sqlite::DataType::Text,
+                    ..Default::default()
+                },
+            ]
+        }
+
+        fn indexes(&self) -> Vec<Index> {
+            todo!()
+        }
+
+        fn create_table_sql(&self) -> String {
+            format!(
+                "create table {} ({});",
+                self.name(),
+                self.columns()
+                    .iter()
+                    .map(|c| c.definition_sql())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+        }
+
+        fn drop_table_sql(&self) -> String {
+            format!("drop table {}", self.name())
+        }
+    }
+
+    #[tokio::test]
+    async fn sync_with_added_columns_works() -> Result<(), sqlx::Error> {
+        let a = TableA::new();
+        let tables = vec![a];
+        let db = db().await;
+        let _ = db.sync(tables).await?;
+        let table_names = db.table_names().await;
+        assert_eq!(1, table_names.len());
+        let a2 = TableA2::new();
+        let tables = vec![a2];
+        let _ = db.sync(tables).await?;
+        let table_names = db.table_names().await;
+        assert_eq!(1, table_names.len());
+        let columns = db.columns().await;
+        assert_eq!(2, columns.len());
         Ok(())
     }
 }
